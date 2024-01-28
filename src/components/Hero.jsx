3ff39fee591bcfdce495af65db47dde892cf3a61
Arfah Ali <FA21-BSE-080@cuiatd.edu.pk>
2024-01-29 00:20:52 +0500

    Added CV

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -100,7 +100,7 @@ const Hero = () => {
             <div className='w-96 h-96 sm:w-72 sm:h-72 md:w-80 md:h-80 lg:w-96 lg:h-96 xl:w-[25rem] xl:h-[25rem] 2xl:w-[32rem] 2xl:h-[32rem]  overflow-hidden rounded-full'>
               <img
                 className='object-cover object-center w-full h-full'
-                src='/arfah.jpg'
+                src='/arfah.png'
                 alt='Social Media Expert'
               />
             </div>
